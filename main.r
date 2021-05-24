a90aaa398c7bdb0f5bc0aecefa9e2bb4549103e7
AmirHossein Habibvand <ahhabibvand@gmail.com>
2021-05-24 20:33:38 +0430

    Add part 3-5

--- main.r
+++ main.r
@@ -1,5 +1,5 @@
 ## Part 0
-df <- read.csv(file = "./dataset/UniversityAdmissions.csv", header = TRUE,  sep = ",", row.names = NULL,  stringsAsFactors = FALSE)
+df <- read.csv(file="./dataset/UniversityAdmissions.csv", header=TRUE,  sep=",", row.names=NULL,  stringsAsFactors=FALSE)
 dim(df)
 colnames(df)[apply(df, 2, anyNA)]
 sum(is.na(df$CGPA))
@@ -18,7 +18,7 @@ ggplot(df, aes(x=GRE.Score)) +
 
 # B.
 qqnorm(colData)
-qqline(colData, col = "red", lwd = 2)
+qqline(colData, col="red", lwd=2)
 
 # C.
 library(e1071)
@@ -69,7 +69,7 @@ quantile(colData)
 boxplot(colData)
 
 
-## Part2
+## Part 2
 colData = df$University.Rating
 
 # A.
@@ -78,7 +78,7 @@ colDf = as.data.frame(
         colData,
         dnn=list("University Rating")
     ),
-    responseName = "freq"
+    responseName="freq"
 )
 
 colDf$percentage = colDf$freq * 100 / sum(colDf$freq)
@@ -86,7 +86,7 @@ colDf$percentage = colDf$freq * 100 / sum(colDf$freq)
 # B.
 ggplot(data=df, aes(x=University.Rating, label=scales::percent(prop.table(stat(count))))) +
     geom_bar(fill=rainbow(length(unique(df$University.Rating)))) +
-    geom_text(stat = 'count', position = position_dodge(.9), vjust = -0.5, size = 3)
+    geom_text(stat='count', position=position_dodge(.9), vjust=-0.5, size=3)
 
 # C.
 library(dplyr)
@@ -96,6 +96,87 @@ ggplot(data=df %>% group_by(University.Rating) %>% summarize(count=n()),
     coord_flip()
 
 # D.
-ggplot(data=df, aes(x=University.Rating, y=Chance.of.Admit, color = University.Rating)) +
+ggplot(data=df, aes(x=University.Rating, y=Chance.of.Admit, color=University.Rating)) +
     geom_violin() +
     geom_boxplot(width=0.1)
+
+
+## Part 3
+
+# B.
+ggplot(df, aes(x=GRE.Score, y=TOEFL.Score)) +
+    geom_point()
+
+# C.
+cor(df$GRE.Score, df$TOEFL.Score)
+
+# E.
+cor.test(df$GRE.Score, df$TOEFL.Score)
+
+# F.
+ggplot(data=df, aes(x=GRE.Score, y=TOEFL.Score, shape=factor(Research))) +
+    geom_point(aes(color=factor(Research)))
+
+# G.
+library(hexbin)
+
+ggplot(data=df, aes(x=GRE.Score, y=TOEFL.Score)) +
+    stat_binhex(bins=10) +
+    scale_fill_gradient(low="blue", high="red")
+
+ggplot(data=df, aes(x=GRE.Score, y=TOEFL.Score)) +
+    stat_binhex(bins=20) +
+    scale_fill_gradient(low="blue", high="red")
+
+
+## Part 4
+
+# A.
+pairs(dplyr::select_if(df, is.numeric))
+
+# C.
+library(scatterplot3d)
+scatterplot3d(
+    df[, c('GRE.Score', 'TOEFL.Score', 'CGPA')],
+    main="3D Scatter Plot",
+    xlab="GRE Score",
+    ylab="TOEFL Score",
+    zlab="CGPA",
+    color=c("red", "blue")[df$Research + 1],
+    pch=16,
+    angle=30
+)
+legend(
+    "bottomright",
+    legend=c("Without Research", "With Research"),
+    col=c("red", "blue"),
+    pch=16
+)
+
+## Part 5
+
+# A.
+frequency_table = table(df$University.Rating, df$internship_abroad)
+margined_frequency_table <- addmargins(frequency_table)
+colnames(margined_frequency_table) <- c("Without Research", "With Research", "Sum")
+print(margined_frequency_table)
+
+# B.
+frequency_table = table(df$internship_abroad, df$University.Rating)
+colors <- c("blue", "red")
+barplot(frequency_table, col=colors, beside=TRUE, xlab="University Rating", ylab="Count")
+legend("topleft", legend=c("Without Research", "With Research"), fill=colors)
+
+# C.
+frequency_table = table(df$internship_abroad, df$University.Rating)
+colors <- c("blue", "red")
+barplot(frequency_table, col=colors, xlab="University Rating", ylab="Count")
+legend("topleft", legend=c("Without Research", "With Research"), fill=colors)
+
+# D.
+manipulated_df <- df
+manipulated_df$Research[manipulated_df$Research==0] <- "Without Research"
+manipulated_df$Research[manipulated_df$Research==1] <- "With Research"
+ggplot(data=manipulated_df) +
+    geom_mosaic(aes(x=product(University.Rating), fill=Research)) +
+    theme_mosaic()
