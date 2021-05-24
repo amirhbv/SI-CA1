6a53c2fe33dfff98722c26cd71ba626572c7b314
AmirHossein Habibvand <ahhabibvand@gmail.com>
2021-05-24 23:38:24 +0430

    Add part 6-9

--- main.r
+++ main.r
@@ -174,9 +174,92 @@ barplot(frequency_table, col=colors, xlab="University Rating", ylab="Count")
 legend("topleft", legend=c("Without Research", "With Research"), fill=colors)
 
 # D.
+library(ggmosaic)
 manipulated_df <- df
 manipulated_df$Research[manipulated_df$Research==0] <- "Without Research"
 manipulated_df$Research[manipulated_df$Research==1] <- "With Research"
 ggplot(data=manipulated_df) +
     geom_mosaic(aes(x=product(University.Rating), fill=Research)) +
     theme_mosaic()
+
+
+## Part 6
+
+# A.
+s <- sample(df$GRE.Score, 100)
+s_mean <- mean(s)
+s_sd <- sd(s)
+SE <- qnorm(0.975) * s_sd / sqrt(length(s))
+CI <- c(s_mean - SE, s_mean + SE)
+cat("95% CI : (", CI, ")\n")
+
+# C.
+ggplot(data=df, aes(x=GRE.Score)) +
+    geom_histogram(binwidth=10, color="black", fill="white") +
+    geom_vline(xintercept=s_mean, size=1.5, color="red") +
+    geom_vline(xintercept=CI[1], size=1.5, color="blue") +
+    geom_vline(xintercept=CI[2], size=1.5, color="blue")
+
+# D.
+h0 <- 320
+SE <- s_sd / sqrt(length(s))
+z_statistic <- (s_mean - h0) / SE
+p_value <- 2 * pnorm(abs(z_statistic), lower.tail=FALSE)
+cat("P-value is ", p_value, "\n")
+
+# F.
+alpha = .05
+sigma = sd(df$GRE.Score)
+sem = sigma / sqrt(length(s));
+mu0 = h0
+I = c(alpha / 2, 1 - alpha / 2)
+q = qnorm(I, mean=mu0, sd=sem);
+p = pnorm(q, mean=mean(df$GRE.Score), sd=sem)
+print(p)
+beta <- diff(p)
+print(beta)
+
+# G.
+power <- 1 - beta
+print(power)
+
+
+## Part 7
+
+# A.
+n <- 25
+sample_population <- df[sample(nrow(df), n),]
+sample_population_var1 <- sample_population$GRE.Score
+sample_population_var2 <- sample_population$TOEFL.Score
+mean_diff <- abs(mean(sample_population_var1) - mean(sample_population_var2))
+t_score <- mean_diff / (sqrt(sd(sample_population_var1) ^ 2 / n) + sqrt(sd(sample_population_var2) ^ 2 / n))
+p_value <- 2 * pt(t_score, df=n - 1, lower.tail=FALSE)
+print(p_value)
+
+# B.
+n <- 100
+sample_population_var1 <- sample(df$GRE.Score, n)
+sample_population_var2 <- sample(df$TOEFL.Score, n)
+mean_diff <- abs(mean(sample_population_var1) - mean(sample_population_var2))
+t_score <- mean_diff / (sqrt(sd(sample_population_var1) ^ 2 / n) + sqrt(sd(sample_population_var2) ^ 2 / n))
+p_value <- 2 * pt(t_score, df=n - 1, lower.tail=FALSE)
+print(p_value)
+
+
+## Part 8
+library(boot)
+cgpa_mean_calculator <- function(data, indices) {
+    return (mean(data[indices, ]$CGPA))
+}
+
+# A.
+boot_results <- boot(df, R=1000, statistic=cgpa_mean_calculator)
+print(boot.ci(boot_results, type="perc"))
+
+# Part B
+boot_results <- boot(df[sample(nrow(df), 20),], R=1000, statistic=cgpa_mean_calculator)
+print(boot.ci(boot_results, type="norm"))
+
+
+## Part 9
+print(summary(aov(df$Chance.of.Admit~as.factor(df$University.Rating))))
